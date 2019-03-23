4617179bbca2ed561fae7b4f719b75db17425cd9
Lahav <Lahav016@gmail.com>
2019-03-23 17:12:37 +0200

    last coding style and code order changes.

--- Exercise 1/MissionContainer.cs
+++ Exercise 1/MissionContainer.cs
@@ -20,7 +20,21 @@ namespace Excercise_1
         {
             FuncMap = new Dictionary<string, Func>();
         }
-
+        
+        /// <summary>
+        ///     Returns a list of the function titles of the mission.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> getAllMissions()
+        {
+            List<string> missions = new List<string>();
+            foreach(var item in FuncMap.Keys)
+            {
+                missions.Add(item);
+            }
+            return missions;
+        }
+        
         /// <summary>
         ///     Override [] for the ability of accessing/changing elements in the dictionary with it.
         /// </summary>
@@ -38,28 +52,13 @@ namespace Excercise_1
                     FuncMap[FuncName] = val => val;
                 }
                 // Return the function that matches the given title.
-                return FuncMap[FuncName];
-                
+                return FuncMap[FuncName]; 
             }
             // FuncMap[FuncName] = Func.
             set
             {
                 FuncMap[FuncName] = value;
             }
-        }
-        
-        /// <summary>
-        ///     Returns a list of the function titles of the mission.
-        /// </summary>
-        /// <returns></returns>
-        public List<string> getAllMissions()
-        {
-            List<string> missions = new List<string>();
-            foreach(var item in FuncMap.Keys)
-            {
-                missions.Add(item);
-            }
-            return missions;
         }
     }
 }
